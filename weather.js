ce4310edf3282f625f7569d54ab5aa9feed4216a
Subrat Rout <subratnrout@gmail.com>
2015-02-24 14:37:46 -0800

    Added ajax call to load city temp data

--- weather.js
+++ weather.js
@@ -3,6 +3,11 @@
  */
 $(document).ready(function(){
     $('form').submit(function(){
-
+        var url = "http://api.openweathermap.org/data/2.5/weather?q=" + $('input').val()+",usa&APPID=23b6315d1d620b5371ceedcf3f3401d3";
+        $.get(url, function(res){
+            var temp =Math.round(((res.main.temp-273.15)*9/5)+32);
+            $('#result').html('<h1>'+ res.name + '</h1><p>'+ temp + '&deg;</p>')
+        }, 'json')
+        return false;
     })
 })
